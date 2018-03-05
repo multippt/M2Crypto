e68fa245979d1d67d6c631529978a34ba2bd4e88
Matěj Cepl <mcepl@cepl.eu>
2018-03-05 13:08:43 +0100

    plat_debian can go without any other issue.

--- tests/__init__.py
+++ tests/__init__.py
@@ -12,6 +12,5 @@ distro_string = linux_distribution(supported_dists=('redhat', 'fedora',
                                                     'debian'),
                                    full_distribution_name=False)[0]
 plat_fedora = distro_string in ['redhat', 'fedora']
-plat_debian = distro_string in ['debian']
 logging.basicConfig(format='%(levelname)s:%(funcName)s:%(message)s',
                     level=logging.DEBUG)
